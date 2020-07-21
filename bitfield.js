483081cd320911200c253a8c1dd2b7974e8e1a8d
rubybb <10256477+rubybb@users.noreply.github.com>
2020-07-21 05:15:54 -0600

    additional impl for bitfield

--- bitfield.js
+++ bitfield.js
@@ -2,8 +2,13 @@
 
 class Bitfield {
 
-  constructor(bits) {
-    this.field = this.constructor.resolve(bits);
+  constructor(bits, flags = {}) {
+    this.field = this.resolve(bits);
+    this.flags = {};
+    
+    Object.keys(flags).forEach(key => {
+      this.flags[key.toUpperCase()] = flags[key];
+    });
   }
 
   is (flag, bool) {
@@ -15,12 +20,25 @@ class Bitfield {
   }
 
   get (flag) {
-      return (this.field & flag) > 0;
+    flag = this.resolve(flag);
+    return (this.field & flag) > 0;
+  }
+
+  toggle (flag) {
+    this.set(flag, !this.get(flag));
+    return this;
+  }
+
+  unset (flag) {
+    this.set(flag, false);
+    return this;
   }
 
   set (flag, bool = true) {
-      bool ? this.field = this.field | flag : this.field &= ~flag; 
-      return this.get(flag);
+    flag = this.resolve(flag);
+
+    bool ? this.field = this.field | flag : this.field &= ~flag; 
+    return this;
   }
 
   toJSON() {  
@@ -31,16 +49,21 @@ class Bitfield {
     return this.field;
   }
 
-  static resolve(bit = 0) {
+  resolve(bit = 0) {
     if (typeof bit === 'number' && bit >= 0) return bit;
     if (bit instanceof Bitfield) return bit.field;
-    if (typeof bit === 'string' && typeof this.FLAGS[bit] !== 'undefined') return this.FLAGS[bit];
 
-    throw new Error('BITFIELD_INVALID');
+    if (typeof bit === "string") {
+      bit = bit.toUpperCase();
+      if (typeof this.flags[bit] !== "undefined") {
+        return this.flags[bit];
+      }
+    }
+
+    throw new Error('invalid bitfield');
   }
 }
 
-
-Bitfield.FLAGS = {};
+//Bitfield.FLAGS = {};
 
 module.exports = Bitfield;
